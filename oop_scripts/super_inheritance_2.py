b2730653feeb804fc6b82ee00f21fd77973549ed
John <jonathan.tremblay.11@gmail.com>
2020-05-10 16:51:27 -0400

    refact repo

--- oop_scripts/super_inheritance_2.py
+++ oop_scripts/super_inheritance_2.py
@@ -2,14 +2,7 @@
 A class that will consider shapes classes
 """
 
-class SurfaceAreaMixin:
-    def surface_area(self):
-        surface_area = 0
-        for surface in self.surfaces:
-            surface_area += surface.area(self)
-
-        return surface_area
-
+class Rectangle:
     def __init__(self, length, width, **kwargs):
         self.length = length
         self.width = width
@@ -18,12 +11,20 @@ class SurfaceAreaMixin:
     def area(self):
         return self.length * self.width
 
-    def perimeter(self):
-        return 2 * self.length + 2 * self.width
+
+class SurfaceAreaMixin:
+    def surface_area(self):
+        surface_area = 0
+        for surface in self.surfaces:
+            surface_area += surface.area(self)
+
+        return surface_area
+
 
 class Square(Rectangle):
     def __init__(self, length, **kwargs):
         super().__init__(length=length, width=length, **kwargs)
+
 
 class Triangle:
     def __init__(self, base, height, **kwargs):
@@ -33,6 +34,7 @@ class Triangle:
 
     def tri_area(self):
         return 0.5 * self.base * self.height
+
 
 class RightPyramid(Square, Triangle):
     def __init__(self, base, slant_height, **kwargs):
@@ -48,11 +50,16 @@ class RightPyramid(Square, Triangle):
         return 0.5 * perimeter * self.slant_height + base_area
 
 
+class Cube(Square, SurfaceAreaMixin):
+    def __init__(self, length):
+        super().__init__(length)
+        self.surfaces = [Square, Square, Square, Square, Square, Square]
+
 # If the python interpreter is running that module (the source file) as the main program,
 # it sets the special __name__ variable to have a value “__main__”.
 if __name__ == "__main__":
-    triangle = Triangle(3, 4)
-    print(Triangle.__mro__)
+    cube = Cube(3)
+    print(cube.surface_area())
 
 else:
     print('Executed when imported')
